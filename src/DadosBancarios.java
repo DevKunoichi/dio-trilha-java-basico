9fc26c7ea3dc7de9ff77db0cd28a411140c53e9c
DevKunoichi <andresa.paula1981@gmail.com>
2023-10-05 16:57:50 -0300

    Desafio Conta Bancária Concluído

--- src/DadosBancarios.java
+++ src/DadosBancarios.java
@@ -1,31 +1,36 @@
 import java.math.BigDecimal;
+
 public class DadosBancarios {
-        private int numeroDaConta;
-        private String agencia;
-        private String nomeDoCliente;
-        private BigDecimal saldo;
+    private int numeroDaConta;
+    private String agencia;
+    private String nomeDoCliente;
+    private BigDecimal saldo;
 
-        public DadosBancarios(int numeroDaConta, String agencia, String nomeDoCliente, BigDecimal saldo) {
-            this.numeroDaConta = numeroDaConta;
-            this.agencia = agencia;
-            this.nomeDoCliente = nomeDoCliente;
-            this.saldo = saldo;
-        }
-
-        public int getNumeroDaConta() {
-            return numeroDaConta;
-        }
-
-        public String getAgencia() {
-            return agencia;
-        }
-
-        public String getNomeDoCliente() {
-            return nomeDoCliente;
-        }
-
-        public BigDecimal getSaldo() {
-            return saldo;
-        }
+    public DadosBancarios(int numeroDaConta, String agencia, String nomeDoCliente, BigDecimal saldo) {
+        this.numeroDaConta = numeroDaConta;
+        this.agencia = agencia;
+        this.nomeDoCliente = nomeDoCliente;
+        this.saldo = saldo;
     }
+
+    public int getNumeroDaConta() {
+
+        return numeroDaConta;
+    }
+
+    public String getAgencia() {
+
+        return agencia;
+    }
+
+    public String getNomeDoCliente() {
+
+        return nomeDoCliente;
+    }
+
+    public BigDecimal getSaldo() {
+
+        return saldo;
+    }
+}
 
